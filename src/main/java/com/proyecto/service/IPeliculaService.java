25acd47d88b25d311114abb02379106304925b83
Darielk51 <94978506+Darielk51@users.noreply.github.com>
2023-04-19 21:50:11 -0600

    Peque

--- src/main/java/com/proyecto/service/IPeliculaService.java
+++ src/main/java/com/proyecto/service/IPeliculaService.java
@@ -4,10 +4,7 @@ package com.proyecto.service;
 import com.proyecto.entity.Pelicula;
 import java.util.List;
 
-/**
- *
- * @author Fernando
- */
+
 public interface IPeliculaService {
     public List<Pelicula> getAllPelicula();
     public Pelicula getPeliculaById (long id);
